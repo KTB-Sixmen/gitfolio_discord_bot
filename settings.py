30c7c25f4a3d556f4954b48cf68eef44265e9885
aida <ghk01214@naver.com>
2024-11-23 14:58:01 +0900

    configmap 설정

---
+++ settings.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    # Discord 설정
+    DISCORD_SERVER_ID: int
+
+    DISCORD_BOT_TOKEN: str
+    DISCORD_BOT_ID: int
+    DISCORD_SENTRY_CHANNEL_ID: int
+
+    HOST: str
+    PORT: int
+
+    PROXY_URL: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+settings = Settings()
